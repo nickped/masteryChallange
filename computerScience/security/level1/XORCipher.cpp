1b9d599df79fda23171e767e94beb11af1fb66bb
nickped <pederson@oregonstate.edu>
2016-12-30 18:57:26 +0000

    removed key file

--- computerScience/security/level1/XORCipher.cpp
+++ computerScience/security/level1/XORCipher.cpp
@@ -4,7 +4,6 @@
  * Error codes:		0: success
  * 			1: ussage messege
  * 			2: error reading files
- * 			3: key is to short for messege
  */
 #include<iostream>
 #include<cstdio>
@@ -13,30 +12,23 @@ using std::endl;
 using std::cout;
 
 int main(int argc, char **argv){
-   if(argc != 3){ //Displaying ussage messege
-      cout <<"Ussage: " <<argv[0] <<" keyFile messegeFile" <<endl;
+   if(argc != 2){ //Displaying ussage messege
+      cout <<"Ussage: " <<argv[0] <<" messegeFile" <<endl;
       return 1;
    }
 
-   FILE *keyPt = fopen(argv[1], "r"), 
-	*filePt = fopen(argv[2], "r");
-   if(keyPt == NULL){ //checking to see if key file is readable
+   FILE *filePt = fopen(argv[1], "r");
+   if(filePt == NULL){ //checking to see if messege file is readable
       cout <<"Error: unable to open " <<argv[1] <<endl;
       return 2;
    }
-   if(filePt == NULL){ //checking to see if messege file is readable
-      cout <<"Error: unable to open " <<argv[2] <<endl;
-      return 2;
-   }
 
-   char currentChar,
-	currentKey;
+   char currentChar;
    
    //Reads first charector of key and messege files   
    currentChar = fgetc(filePt);
-   currentKey = fgetc(keyPt);
 
-   while(!feof(filePt) && !feof(keyPt)){
+   while(!feof(filePt)){
       if(currentChar == '\n') cout <<endl; //keeps new lines in coded messege and flushes buffer
       else{
 	 cout <<(char)~currentChar; //Prints currentChar XOR currentKey
@@ -44,13 +36,6 @@ int main(int argc, char **argv){
 
       //Reads next charectors
       currentChar = fgetc(filePt);
-      currentKey = fgetc(keyPt);
-   }
-
-   //checking for bad key
-   if(!feof(filePt) && feof(keyPt)){
-      cout <<"\nError: bad key\n";
-      return 3;
    }
    return 0;
 }
